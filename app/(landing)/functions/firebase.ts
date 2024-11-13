b787009fe433bfd07f5c3d3ade46ca3d9d1f90b9
BJChickky12 <bjchickky@gmail.com>
2024-11-13 23:21:41 +0800

    config

--- app/(landing)/functions/firebase.ts
+++ app/(landing)/functions/firebase.ts
@@ -16,7 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 
 export const db = getFirestore(app);
 
