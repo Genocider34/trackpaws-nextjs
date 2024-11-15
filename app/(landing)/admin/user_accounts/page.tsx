fc2779cb834f4838a43aa51a44dac489f7815574
BJChickky12 <bjchickky@gmail.com>
2024-11-15 23:15:21 +0800

    removed not used libraries

--- app/(landing)/admin/user_accounts/page.tsx
+++ app/(landing)/admin/user_accounts/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, {useEffect, useState} from 'react';
-import { collection, Timestamp, onSnapshot, updateDoc, doc, deleteDoc } from 'firebase/firestore';
+import { collection, Timestamp, onSnapshot } from 'firebase/firestore';
 import { db } from '../../functions/firebase';
 
 interface UserAccounts {
